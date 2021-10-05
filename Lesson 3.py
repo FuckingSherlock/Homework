58153146ea40122525345d97011796a6dc5470c1
Kirill <FuckingSherlock@users.noreply.github.com>
2021-10-05 22:21:25 +0300

    Работа 1

--- Lesson 3.py
+++ Lesson 3.py
@@ -1,14 +1,24 @@
-nm = input('Имя:')
-snm = input('Фамилия:')
-age = int(input('Возраст:'))
-wt = int(input('Вес:'))
-if (age <= 30) and (50 <= wt <= 120):
-    print(nm, snm+',', age, 'год, вес', wt, '- хорошее состояние')
-elif (age <= 30) and (50 > wt or wt > 120):
-    print(nm, snm+',', age, 'год, вес', wt, '- следует правильно питаться')
-elif (age > 30) and (wt < 50 or wt > 120):
-    print(nm, snm+',', age, 'год, вес', wt, '- требуется заняться собой')
-elif (age >= 40) and (wt < 50 or wt > 120):
-    print(nm, snm+',', age, 'год, вес', wt, '- следует обратится к врачу!')
-else:
-    print('Пликлиника закрыта на карантин')
+# duration = 53
+# 53 сек
+# duration = 153
+# 2 мин 33 сек
+# duration = 4153
+# 1 час 9 мин 13 сек
+# duration = 400153
+# 4 дн 15 час 9 мин 13 сек
+duration = int(input())
+m = duration//60
+h = duration//3600
+d = duration // 86400
+if duration < 60:
+    s = duration
+    print(s, 'сек')
+elif 3600 > duration >= 60:
+    s = duration % 60
+    print(m, 'мин', s, 'сек')
+elif 86400 > duration >= 3600:
+    s = duration % 60
+    print(h, 'час', m, 'мин', s, 'сек')
+elif duration >= 86400:
+    s = duration % 60
+    print(d, 'дн', h, 'час', m, 'мин', s, 'сек')
