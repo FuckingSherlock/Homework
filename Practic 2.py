06b38699e5013fb1dfb278b69190bf4fe342c613
Kirill <FuckingSherlock@users.noreply.github.com>
2021-10-07 07:49:18 +0300

    some

--- Practic 2.py
+++ Practic 2.py
@@ -3,7 +3,6 @@ nums = list(range(1, 1001, 2))
 coubs = [i**3 for i in nums]
 sum_of_digits = []
 sum_of_digits_17 = []
-temp = [sum(map(int, str(i))) for i in coubs]
 for i in coubs:
     if sum(map(int, str(i))) % 7 == 0:
         sum_of_digits.append(i)
