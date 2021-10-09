7fa89e2cde121dd93a1938b034bf7c850e7fdb91
Kirill <FuckingSherlock@users.noreply.github.com>
2021-10-10 01:56:01 +0300

    Fixed

--- Lesson 2/Num 2.py
+++ Lesson 2/Num 2.py
@@ -1,6 +1,5 @@
 info = ['в', '5', 'часов', '17', 'минут',
         'температура', 'воздуха', 'была', '+5', 'градусов']
-indexes = []
 new_info = []
 for i in info:
     temp = list(i)
@@ -9,16 +8,8 @@ for i in info:
             if int(''.join(temp)) < 10:
                 temp.insert(-1, '0')
                 i = ''.join(temp)
-                # indexes.append(info.index(i))
-            # else:
-            #     # indexes.append(info.index(i))
-            # print(i)
-            new_info.extend(['"', i, '"', ' '])
-        else:
-            new_info.extend([i, ' '])
+            i = ''.join(['"', i, '"', ' '])
+            break
+    i = ''.join([i, ' '])
+    new_info.append(i)
 print(''.join(new_info))
-#     if info.index(i) in indexes:
-#         new_info.extend(['"', i, '"', ' '])
-#     else:
-#         new_info.extend([i, ' '])
-# print(''.join(new_info))
