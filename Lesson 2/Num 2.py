15545bdb65e79798908385b2faff8bc5660c6533
Kirill <FuckingSherlock@users.noreply.github.com>
2021-10-09 16:14:25 +0300

    не работает

--- Lesson 2/Num 2.py
+++ Lesson 2/Num 2.py
@@ -6,22 +6,19 @@ for i in info:
     temp = list(i)
     for any in temp:
         if any.isdigit():
-            indexes.append(info.index(i))
-
-    if info.index(i) in indexes:
-        i = int(i)
-        if i < 10:
-            temp.insert((temp.index(i)-4), '0')
-            info[i] = ''.join(temp)
-        i = str(i)
-        new_info.extend(['"', i, '"', ' '])
-    else:
-        new_info.extend([i, ' '])
-sring = ''.join(new_info)
-print(sring)
-
-# ['в', '"', '05', '"', 'часов', '"', '17', '"', 'минут',
-#   'температура', 'воздуха', 'была', '"', '+05', '"', 'градусов']
-
-
-# в "05" часов "17" минут температура воздуха была "+05" градусов
+            if int(''.join(temp)) < 10:
+                temp.insert(-1, '0')
+                i = ''.join(temp)
+                # indexes.append(info.index(i))
+            # else:
+            #     # indexes.append(info.index(i))
+            # print(i)
+            new_info.extend(['"', i, '"', ' '])
+        else:
+            new_info.extend([i, ' '])
+print(''.join(new_info))
+#     if info.index(i) in indexes:
+#         new_info.extend(['"', i, '"', ' '])
+#     else:
+#         new_info.extend([i, ' '])
+# print(''.join(new_info))
