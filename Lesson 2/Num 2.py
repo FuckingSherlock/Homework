e9813b0b5917f26b6c0953c8a9132e894983ccd6
Kirill <FuckingSherlock@users.noreply.github.com>
2021-10-10 16:54:16 +0300

    fixed

--- Lesson 2/Num 2.py
+++ Lesson 2/Num 2.py
@@ -2,14 +2,12 @@ info = ['в', '5', 'часов', '17', 'минут',
         'температура', 'воздуха', 'была', '+5', 'градусов']
 new_info = []
 for i in info:
-    temp = list(i)
-    for any in temp:
-        if any.isdigit():
-            if int(''.join(temp)) < 10:
-                temp.insert(-1, '0')
-                i = ''.join(temp)
-            i = ''.join(['"', i, '"', ' '])
-            break
+    if list(i).pop(-1).isdigit():
+        if int(i) < 10:
+            i = list(i)
+            i.insert(-1, '0')
+            i = ''.join(i)
+        i = ''.join(['"', i, '"', ' '])
     i = ''.join([i, ' '])
     new_info.append(i)
 print(''.join(new_info))
