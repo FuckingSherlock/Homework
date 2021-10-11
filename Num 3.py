2ad6f68f61b7ca8c961867446a4e0793c483bee4
Kirill <FuckingSherlock@users.noreply.github.com>
2021-10-12 02:45:07 +0300

    update

--- Num 3.py
+++ Num 3.py
@@ -4,8 +4,8 @@ index = 0
 while index in range(len(info)):
     if info[index][0:].isdigit() or info[index][1:].isdigit():
         if len(info[index]) < 2:
-            info[index] = f'{info[index][1:]}0{info[index][:1]}'
-        elif int(info[index]) < 10:
+            info[index] = '0' + info[index][:1]
+        elif (int(info[index]) or (-int(info[index]))) < 10:
             info[index] = f'{info[index][:1]}0{info[index][1:]}'
         info[index] = ''.join(['"', info[index], '"', ' '])
     info[index] = ''.join([info[index], ' '])
