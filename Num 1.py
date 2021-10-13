2264da5a261023a1a942f0f1280f5f113adb076d
Kirill <FuckingSherlock@users.noreply.github.com>
2021-10-14 02:32:54 +0300

    Слишком просто....

---
+++ Num 1.py
@@ -0,0 +1,13 @@
+nums = {'zero': 'ноль', 'one': 'один', 'two': 'два', 'three': 'три', 'four': 'четыре',
+        'five': 'пять', 'six': 'шесть', 'seven': 'семь', 'eight': 'восемь', 'nine': 'девять', 'ten': 'десять'}
+
+
+def num_translate(eng):
+    if eng in nums:
+        ru = nums[eng]
+        print(ru)
+    else:
+        print(None)
+
+
+num_translate(input())
