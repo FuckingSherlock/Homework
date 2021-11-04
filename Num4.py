25cde818693d752385c1fad5a20757a958590538
Kirill <FuckingSherlock@users.noreply.github.com>
2021-11-04 17:17:28 +0300

    last

--- Num4.py
+++ Num4.py
@@ -1,10 +1,16 @@
 from functools import wraps
 
 
-def val_checker(func):
-    if func == True:
-        print('yes')
-    return func
+def val_checker(decor_func):
+    def _val_checker(func_clack):
+        @wraps(func_clack)
+        def wrapped(x):
+            if decor_func(x):
+                return func_clack(x)
+            else:
+                raise ValueError(x)
+        return wrapped
+    return _val_checker
 
 
 @val_checker(lambda x: x > 0)
@@ -12,13 +18,4 @@ def calc_cube(x):
     return x ** 3
 
 
-calc_cube(5)
-
-# >>> a = calc_cube(5)
-# 125
-# >>> a = calc_cube(-5)
-# Traceback (most recent call last):
-#   ...
-#     raise ValueError(msg)
-# ValueError: wrong val -5
-#
+print(calc_cube(-5))
